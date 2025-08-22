a9c01c6f5685cc9a2a45deee53dfb76684aa0d8a
Benjamin Esquieres <40147170+delta-12@users.noreply.github.com>
2025-08-22 00:02:58 -0400

    NEX-13 menus (#3)

--- main.py
+++ main.py
@@ -1,51 +1,13 @@
-from deploy import Deployment
-from environment import ContainerEnvironment, Images
-from steps import (
-    AddDomainToCertificate,
-    BuildNginxReverseProxyConfig,
-    GitClone,
-    Properties,
-    ReadNexusConfig,
-    ReloadNginx,
-    TestNginxConfig,
-)
-import logging
-
-LOGGER = logging.getLogger(__name__)
-REVERSR_PROXY_NAME = "nexus-reverse-proxy"
-
-logging.basicConfig(level=logging.INFO)
+from menu import MenuContext
+from menus import MainMenu
 
 
 def main() -> int:
-    repo = input("Enter repo to deploy: ")
-    reverse_proxy_environment = ContainerEnvironment(
-        container_name=REVERSR_PROXY_NAME,
-        container_image=Images.REVERSE_PROXY,
-        container_ports={"80/tcp": 80, "443/tcp": 443},
-    )
-    reverse_proxy_deployment = Deployment(reverse_proxy_environment, REVERSR_PROXY_NAME)
-    environment = ContainerEnvironment()
-    deployment = Deployment(environment)
-    deployment.add_step(GitClone(repo))
-    deployment.add_step(ReadNexusConfig())
-    exit_code, output = deployment.run_all_steps()
+    menu_context = MenuContext()
+    menu_context.add_menu(MainMenu())
+    menu_context.show()
 
-    if 0 == exit_code:
-        name = deployment.get_property(Properties.NAME)
-        domain = deployment.get_property(Properties.DOMAIN)
-        email = deployment.get_property(Properties.EMAIL)
-        reverse_proxy_deployment.add_step(AddDomainToCertificate(domain, email))
-        reverse_proxy_deployment.add_step(BuildNginxReverseProxyConfig(domain, name))
-        reverse_proxy_deployment.add_step(TestNginxConfig())
-        reverse_proxy_deployment.add_step(ReloadNginx())
-        exit_code, output = reverse_proxy_deployment.run_all_steps()
-
-    if 0 != exit_code:
-        # TODO kill container and prune if failure
-        print(f"Exit code: {exit_code}, Error message {output}")
-
-    return exit_code
+    return 0
 
 
 if __name__ == "__main__":
